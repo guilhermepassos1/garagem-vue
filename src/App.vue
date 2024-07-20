849a580cc10ea422d7c6b4ae0405629c1b0376e5
Guilherme Passos <107271443+guilhermepassos1@users.noreply.github.com>
2024-07-19 21:24:22 -0300

    Atualizar o App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
-import router from './router';
 <template>
   <header>
     <router-link :to="{ name: 'home' }">Home</router-link> |
-    <router-link to="/categorias">Categorias</router-link>
-    <router-link to="/cores">Cores</router-link>
-    <router-link to="/marcas">Marcas</router-link>
-    <router-link to="/acessorios">Acessorios</router-link>
+    <router-link to="/categorias">Categorias</router-link> |
+    <router-link to="/cores">Cores</router-link> |
+    <router-link to="/marcas">Marcas</router-link> |
+    <router-link to="/acessorios">Acessorios</router-link> |    
+    <router-link to="/login">Login</router-link>
+
   </header>
   <main>
     <RouterView />
